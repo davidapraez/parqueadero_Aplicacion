5c27260ea2eb39e806e79bf38c868e90037c91b3
David Apraez <omarapraez@hotmail.com>
2023-06-25 16:07:25 -0500

    ss

--- connecDB.php
+++ connecDB.php
@@ -1,6 +1,6 @@
 <?php
 function conectar(){
-    $bd = mysqli_connect("localhost","root","","Parqueadero");
+    $bd = mysqli_connect("34.233.135.157","user","userpasword","parqueadero");
     if (!$bd){
         echo "<h3>Conexión no realizada<h3>";
         return NULL;
